347e1e89dd6351648eed1ae4376581f68e441379
yashdev9274 <yashdev.yvd@gmail.com>
2024-03-27 19:10:05 +0530

    comment out trpc provider

--- component/Providers.tsx
+++ component/Providers.tsx
@@ -1,41 +1,41 @@
-"use client";
+// "use client";
 
-import { trpc } from "@/app/_trpc/client"
-import {
-    QueryClient,
-    QueryClientProvider,
-} from '@tanstack/react-query'
-import { httpBatchLink } from '@trpc/client'
-// import { absoluteUrl } from '@/lib/utils'
+// import { trpc } from "@/app/_trpc/client"
+// import {
+//     QueryClient,
+//     QueryClientProvider,
+// } from '@tanstack/react-query'
+// import { httpBatchLink } from '@trpc/client'
+// // import { absoluteUrl } from '@/lib/utils'
 
-import { PropsWithChildren, useState } from "react"
+// import { PropsWithChildren, useState } from "react"
 
-const Providers = ({ children }: PropsWithChildren) => {
+// const Providers = ({ children }: PropsWithChildren) => {
 
-    const [queryClient] = useState(() => new QueryClient())
-    const [trpcClient] = useState(() =>
-        trpc.createClient({
-            links: [
-                httpBatchLink({
-                    // url: absoluteUrl("/api/trpc"),
-                    url: "http://localhost:3000/api/trpc",
+//     const [queryClient] = useState(() => new QueryClient())
+//     const [trpcClient] = useState(() =>
+//         trpc.createClient({
+//             links: [
+//                 httpBatchLink({
+//                     // url: absoluteUrl("/api/trpc"),
+//                     url: "http://localhost:3000/api/trpc",
 
-                })
-            ]
-        })
-    )
+//                 })
+//             ]
+//         })
+//     )
 
-    return (
+//     return (
 
-        <trpc.Provider
-            client={trpcClient}
-            queryClient={queryClient}
-        >
-            <QueryClientProvider client={queryClient}>
-                {children}
-            </QueryClientProvider>
-        </trpc.Provider>
-    )
-}
+//         <trpc.Provider
+//             client={trpcClient}
+//             queryClient={queryClient}
+//         >
+//             <QueryClientProvider client={queryClient}>
+//                 {children}
+//             </QueryClientProvider>
+//         </trpc.Provider>
+//     )
+// }
 
-export default Providers
+// export default Providers
