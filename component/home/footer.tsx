5b95ff7550b43cfefd5ebb140aa1cf276ed5da06
yashdev9274 <yashdev.yvd@gmail.com>
2024-05-13 13:47:05 +0530

    feat: update footer component, elements are aligned.

--- component/home/footer.tsx
+++ component/home/footer.tsx
@@ -17,6 +17,15 @@ const navigation = {
         { name: "Pricing", href: "/pricing" },
         { name: "Help Center", href: "/help" },
     ],
+    resources: [
+        { name: "Blog", href: "/blog" },
+        { name: "Brand", href: "/brand" },
+        { name: "Changelog", href: "/changelog" },
+        { name: "Customers", href: "/customers" },
+        { name: "Enterprise", href: "/enterprise" },
+        { name: "Pricing", href: "/pricing" },
+        { name: "Help Center", href: "/help" },
+    ],
 
 };
 
@@ -36,7 +45,7 @@ export function Footer() {
 
         <footer className="border-t-[1px] border-border px-4 md:px-6 pt-10 md:pt-16 bg-[#F6F6F3] ">
             <MaxWidthWrapper className="relative z-10 overflow-hidden border border-b-0 border-gray-200 bg-white/50 pb-60 pt-16 backdrop-blur-lg md:rounded-t-2xl">
-                <div className="xl:grid xl:grid-cols-3 xl:gap-8">
+                <div className="xl:grid xl:grid-cols-3 xl:gap-5 px-8">
                     <div className="space-y-6">
 
                         <Link href="/" className="scale-50 -ml-[52px] md:ml-0 md:scale-100">
@@ -59,8 +68,8 @@ export function Footer() {
                             </div>
                         </div>
                     </div>
-                    <div className="mt-16 grid grid-cols-2 gap-4 xl:col-span-2 xl:mt-0">
-                        <div className="md:grid md:grid-cols-2">
+                    <div className="mt-16 grid grid-cols-2 gap-2 xl:col-span-2 xl:mt-0">
+                        <div className="mt-16 grid grid-cols-1 md:grid-cols-2  xl:col-span-2 xl:mt-0 px-8">
                             <div>
                                 <h3 className="text-sm font-semibold text-gray-800">
                                     Product
@@ -86,10 +95,10 @@ export function Footer() {
                                     ))}
                                 </ul>
                             </div>
-                            <div className="mt-10 md:mt-0">
-                                <h3 className="text-sm font-semibold text-gray-800">Product</h3>
-                                <ul role="list" className="mt-4 space-y-4">
-                                    {navigation.product.map((item) => (
+                            <div className="mt-4 md:mt-0">
+                                <h3 className="text-sm font-semibold text-gray-800">Resources</h3>
+                                <ul role="list" className="mt-1 space-y-4">
+                                    {navigation.resources.map((item) => (
                                         <li key={item.name}>
                                             <Link
                                                 href={createHref(item.href)}
@@ -114,7 +123,7 @@ export function Footer() {
                 </div>
 
 
-                <p className="text-xs text-[#000000] dark:text-[#3E3E3E] mb-50">
+                <p className="text-xs text-[#000000] dark:text-[#3E3E3E] mb-50 px-5 mt-5">
                     @2024 DocandDraw
                 </p>
             </MaxWidthWrapper>
