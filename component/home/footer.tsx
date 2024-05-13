941b8abe6c684e376b84eff4da9765a2bd9ac03c
yashdev9274 <yashdev.yvd@gmail.com>
2024-05-13 13:21:19 +0530

    feat: update footer component and add maxWidthWrapper.

--- component/home/footer.tsx
+++ component/home/footer.tsx
@@ -1,10 +1,24 @@
 "use client";
 
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useParams } from "next/navigation";
 import { SocialLinks } from "@/component/home/social-links";
 import { LogoLarge } from "./logo-large";
-// import { StatusWidget } from "./status-widget";
+import MaxWidthWrapper from "../MaxWidthWrapper";
+
+
+const navigation = {
+    product: [
+        { name: "Blog", href: "/blog" },
+        { name: "Brand", href: "/brand" },
+        { name: "Changelog", href: "/changelog" },
+        { name: "Customers", href: "/customers" },
+        { name: "Enterprise", href: "/enterprise" },
+        { name: "Pricing", href: "/pricing" },
+        { name: "Help Center", href: "/help" },
+    ],
+
+};
 
 export function Footer() {
     const pathname = usePathname();
@@ -13,12 +27,18 @@ export function Footer() {
         return null;
     }
 
-    return (
-        <>
+    const { domain = "https://docndraw.vercel.app" } = useParams() as { domain: string };
 
-            <footer className="border-t-[1px] border-border px-4 md:px-6 pt-10 md:pt-16 bg-[#F6F6F3] dark:bg-[#0C0C0C]">
-                <div className="container">
-                    <div className="flex justify-between items-center border-border border-b-[1px] pb-10 md:pb-16 mb-12">
+    const createHref = (href: string) =>
+        domain === "https://docndraw.vercel.app" ? href : `https://docndraw.vercel.app${href}`;
+
+    return (
+
+        <footer className="border-t-[1px] border-border px-4 md:px-6 pt-10 md:pt-16 bg-[#F6F6F3] ">
+            <MaxWidthWrapper className="relative z-10 overflow-hidden border border-b-0 border-gray-200 bg-white/50 pb-60 pt-16 backdrop-blur-lg md:rounded-t-2xl">
+                <div className="xl:grid xl:grid-cols-3 xl:gap-8">
+                    <div className="space-y-6">
+
                         <Link href="/" className="scale-50 -ml-[52px] md:ml-0 md:scale-100">
                             {/* <LogoLarge /> */}
                             <span className="sr-only">Doc&Draw</span>
@@ -27,97 +47,78 @@ export function Footer() {
                         <span className="font-normal md:text-2xl text-right">
                             Stand out as a Developer.
                         </span>
-                    </div>
-
-                    <div className="flex flex-col md:flex-row w-full mb-10 md:mb-20">
-                        <div className="flex flex-col space-y-8 md:space-y-0 md:flex-row md:w-6/12 justify-between leading-8">
-                            <div>
-                                <span className="font-medium">Product</span>
-                                <ul>
-                                    <li className="text-[#707070] transition-colors hover:text-primary dark:text-[#878787]">
-                                        <Link href="/">Features</Link>
-                                    </li>
-                                    <li className="text-[#707070] transition-colors hover:text-primary dark:text-[#878787]">
-                                        <Link href="/pricing">Pricing</Link>
-                                    </li>
-                                    <li className="text-[#707070] transition-colors hover:text-primary dark:text-[#878787]">
-                                        <Link href="/story">Story</Link>
-                                    </li>
-                                    <li className="text-[#707070] transition-colors hover:text-primary dark:text-[#878787]">
-                                        <Link href="/updates">Updates</Link>
-                                    </li>
-                                    <li className="text-[#707070] transition-colors hover:text-primary dark:text-[#878787]">
-                                        <Link href="/download">Download</Link>
-                                    </li>
-                                    <li className="text-[#707070] transition-colors hover:text-primary dark:text-[#878787]">
-                                        <Link href="/feature-request">Feature Request</Link>
-                                    </li>
-                                </ul>
-                            </div>
-
-                            <div>
-                                <span>Resources</span>
-                                <ul>
-                                    <li className="text-[#707070] transition-colors hover:text-primary dark:text-[#878787]">
-                                        <Link href="https://git.new/docndraw">Github</Link>
-                                    </li>
-                                    <li className="text-[#707070] transition-colors hover:text-primary dark:text-[#878787]">
-                                        <Link href="/support">Support</Link>
-                                    </li>
-                                    <li className="text-[#707070] transition-colors hover:text-primary dark:text-[#878787]">
-                                        <Link href="/policy">Privacy policy</Link>
-                                    </li>
-                                    <li className="text-[#707070] transition-colors hover:text-primary dark:text-[#878787]">
-                                        <Link href="/terms">Terms and Conditions</Link>
-                                    </li>
-                                    <li className="text-[#707070] transition-colors hover:text-primary dark:text-[#878787]">
-                                        <Link href="/open-startup">Open Startup</Link>
-                                    </li>
-                                    <li className="text-[#707070] transition-colors hover:text-primary dark:text-[#878787]">
-                                        <Link href="/pitch">Investors</Link>
-                                    </li>
-                                </ul>
-                            </div>
-
-                            <div>
-                                <span>Solutions</span>
-                                <ul>
-                                    <li className="text-[#707070] transition-colors hover:text-primary dark:text-[#878787]">
-                                        <Link href="https://docs.docndraw/">
-                                            Self hosted
-                                        </Link>
-                                    </li>
-                                    <li className="text-[#707070] transition-colors hover:text-primary dark:text-[#878787]">
-                                        <Link href="/">SaaS hosting</Link>
-                                    </li>
-                                    {/* Nothing here yet */}
-                                    {/* <li className="text-[#707070] transition-colors hover:text-primary dark:text-[#878787]">
-                  <Link href="/open-startup">Open startup</Link>
-                </li> */}
-                                    <li className="text-[#707070] transition-colors hover:text-primary dark:text-[#878787]">
-                                        <Link href="/oss-friends">OSS friends</Link>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-
                         <div className="md:w-6/12 flex mt-8 md:mt-0 md:justify-end">
                             <div className="flex justify-between md:items-end flex-col space-y-14">
                                 <div className="flex items-center">
+                                    {/* <GithubStars /> */}
                                     <SocialLinks />
                                 </div>
                                 <div className="md:mr-0 mr-auto">
+                                    {/* <StatusWidget /> */}
                                 </div>
                             </div>
                         </div>
                     </div>
-
-                    <p className="text-xs text-[#000000] dark:text-[#3E3E3E] mb-50">
-                        @2024 DocandDraw
-                    </p>
+                    <div className="mt-16 grid grid-cols-2 gap-4 xl:col-span-2 xl:mt-0">
+                        <div className="md:grid md:grid-cols-2">
+                            <div>
+                                <h3 className="text-sm font-semibold text-gray-800">
+                                    Product
+                                </h3>
+                                <ul role="list" className="mt-4 space-y-4">
+                                    {navigation.product.map((item) => (
+                                        <li key={item.name}>
+                                            <Link
+                                                href={createHref(item.href)}
+                                                {...(domain !== "https://docndraw.vercel.app" && {
+                                                    // onClick: () => {
+                                                    //     va.track("Referred from custom domain", {
+                                                    //         domain,
+                                                    //         medium: `footer item (${item.name})`,
+                                                    //     });
+                                                    // },
+                                                })}
+                                                className="text-sm text-gray-500 hover:text-gray-800"
+                                            >
+                                                {item.name}
+                                            </Link>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                            <div className="mt-10 md:mt-0">
+                                <h3 className="text-sm font-semibold text-gray-800">Product</h3>
+                                <ul role="list" className="mt-4 space-y-4">
+                                    {navigation.product.map((item) => (
+                                        <li key={item.name}>
+                                            <Link
+                                                href={createHref(item.href)}
+                                                {...(domain !== "https://docndraw.vercel.app" && {
+                                                    // onClick: () => {
+                                                    //     va.track("Referred from custom domain", {
+                                                    //         domain,
+                                                    //         medium: `footer item (${item.name})`,
+                                                    //     });
+                                                    // },
+                                                })}
+                                                className="text-sm text-gray-500 hover:text-gray-800"
+                                            >
+                                                {item.name}
+                                            </Link>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </footer>
-        </>
+
+
+                <p className="text-xs text-[#000000] dark:text-[#3E3E3E] mb-50">
+                    @2024 DocandDraw
+                </p>
+            </MaxWidthWrapper>
+        </footer>
 
     );
 }
