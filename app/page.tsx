43633a54528a5fb2d56730cbe68304f57fe7d34f
yashdev9274 <yashdev.yvd@gmail.com>
2024-05-19 18:50:39 +0530

    feat: add card shuffle feature using framer motion for screens and updated screen image.

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import dashboard from "@/public/asset/dashboard.png";
+import screen1 from "@/public/asset/screen1.png";
 import { DockMenu } from "./_components/dockMenu";
 import { Footer } from "@/component/home/footer";
 import Hero from "./_components/Hero";
@@ -13,6 +14,7 @@ import Feature from "@/component/home/feature-section";
 import Companies from "@/component/home/social-proof-companies";
 import { Testimonials } from "@/component/home/testimonial-grid";
 import { Stats } from "@/component/home/stats";
+import { Screens } from "@/component/home/screens";
 
 
 
@@ -27,15 +29,17 @@ export default function Home() {
 
       <div className="flex justify-center items-center relative  md:mt-[-60px]">
         <Image
-          src={dashboard}
+          src={screen1}
           alt="banner image"
-          height={1200}
-          width={1200}
+          width={1031}
+          height={670}
           className="rounded-tl-2xl rounded-tr-2xl border-2 border-muted"
         />
         <div className="bottom-0 top-[50%] bg-gradient-to-t dark:from-background left-0 right-0 absolute z-10"></div>
         <BorderBeam size={250} duration={12} delay={9} />
       </div>
+
+      {/* <Screens /> */}
 
       <DockMenu />
       <Companies />
