b8092dd33d939499f6a694c802a31c0157a43947
yashdev9274 <yashdev.yvd@gmail.com>
2024-05-19 12:01:21 +0530

    feat: update feature section in landing page.

--- component/home/feature-section.tsx
+++ component/home/feature-section.tsx
@@ -6,77 +6,78 @@ const Feature = () => {
     const features = [
         {
             icon:
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-11 h-6">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z" />
                 </svg>,
-            title: "Fast Refresh",
-            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue, nisl eget molestie varius, enim ex faucibus purus."
+            title: "Real-Time Co-editing",
+            desc: "Collaborate in real-time with your team, no matter where they are. Doc&Draw's intuitive interface and powerful co-editing capabilities ensure smooth and efficient collaboration, eliminating version conflicts and streamlining your workflow."
         },
         {
             icon:
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z" />
                 </svg>,
-            title: "Analytics",
-            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue, nisl eget molestie varius, enim ex faucibus purus."
+            title: "Robust Annotation and Drawing Tools",
+            desc: "Express your ideas with clarity and precision using Doc&Draw's comprehensive annotation and drawing tools. From freehand sketching to precise vector drawing, Doc&Draw empowers you to bring your visions to life with ease."
         },
         {
             icon:
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
                 </svg>,
-            title: "Datacenter security",
-            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue, nisl eget molestie varius, enim ex faucibus purus."
+            title: "Secure and Private by Design",
+            desc: "Protect your sensitive data and intellectual property with Doc&Draw's robust security measures. Benefit from end-to-end encryption, granular access controls, and advanced authentication methods, ensuring your work remains confidential and secure."
         },
         {
             icon:
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M6.429 9.75L2.25 12l4.179 2.25m0-4.5l5.571 3 5.571-3m-11.142 0L2.25 7.5 12 2.25l9.75 5.25-4.179 2.25m0 0L21.75 12l-4.179 2.25m0 0l4.179 2.25L12 21.75 2.25 16.5l4.179-2.25m11.142 0l-5.571 3-5.571-3" />
                 </svg>,
-            title: "Build on your terms",
-            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue, nisl eget molestie varius, enim ex faucibus purus."
+            title: "Cross-Platform Compatibility",
+            desc: "Access and collaborate on your projects from anywhere, on any device. Doc&Draw's cross-platform compatibility ensures a consistent and familiar experience, whether you're working on a desktop, tablet, or mobile device."
         },
         {
             icon:
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75L11.25 15 15 9.75m-3-7.036A11.959 11.959 0 013.598 6 11.99 11.99 0 003 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285z" />
                 </svg>,
-            title: "Safe to use",
-            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue, nisl eget molestie varius, enim ex faucibus purus."
+            title: "Open-Source and Customizable",
+            desc: "As an open-source solution, Doc&Draw offers unparalleled flexibility and transparency. Customize the platform to meet your specific needs, contribute to its development, or simply benefit from the collective efforts of a vibrant community."
         },
         {
             icon:
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z" />
                 </svg>,
-            title: "Flexible",
-            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue, nisl eget molestie varius, enim ex faucibus purus."
+            title: "Seamless Integration",
+            desc: "Seamlessly integrate Doc&Draw into your existing workflow and tools. With support for a wide range of file formats and compatibility with popular cloud storage providers, Doc&Draw fits seamlessly into your existing ecosystem."
         },
     ]
 
     return (
         <section className="
-        h-full w-full pt11 
+        h-full w-full mt-2
         relative flex items-center 
         justify-center flex-col bg-[#F6F6F3]">
             <div className="max-w-screen-xl mx-auto px-4 text-gray-600 md:px-8">
-                <div className="max-w-xl mx-auto space-y-7 sm:text-center">
-                    <h3 className="text-indigo-600 font-semibold pb-5">
+                <div className="max-w-xl mx-auto space-y-3 sm:text-center">
+                    <h3 className="text-5xl text-indigo-600 font-semibold pb-5">
                         Features
                     </h3>
-                    <p className="text-gray-800 text-3xl font-semibold sm:text-4xl pb-3">
-                        Do more with less complexity
+                    <p className="text-gray-800 text-xs	 font-semibold sm:text-4xl pb-5">
+                        Seamless Collaboration and Effortless Creativity
+
                     </p>
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec congue, nisl eget molestie varius, enim ex faucibus purus
+                        Doc&Draw empowers teams to collaborate seamlessly, fostering innovation and productivity. Harness the power of real-time editing, intuitive tools, and robust security features to unleash your creative potential.
                     </p>
                 </div>
                 <div className="mt-10">
-                    <ul className="grid pb-5 gap-y-20 gap-x-12 sm:grid-cols-2 lg:grid-cols-3">
+                    <ul className="grid pb-5 gap-y-20 gap-x-20 sm:grid-cols-2 lg:grid-cols-3">
                         {
                             features.map((item, idx) => (
-                                <li key={idx} className="flex gap-x-4">
-                                    <div className="flex-none w-20 h-12 bg-indigo-50 text-indigo-600 rounded-lg flex items-center justify-center">
+                                <li key={idx} className="flex gap-x-4 mt-5">
+                                    <div className="flex-none w-80 h-10 bg-indigo-50 text-indigo-600 rounded-lg flex items-center justify-center">
                                         {item.icon}
                                     </div>
                                     <div>
