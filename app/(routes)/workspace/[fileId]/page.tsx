85008fbd9b56a2e6d2ba629436b7b6bb3555b86e
yashdev9274 <yashdev.yvd@gmail.com>
2024-06-21 00:29:31 +0530

    ⭐ feat: update workspace layout, added resizable component.

--- app/(routes)/workspace/[fileId]/page.tsx
+++ app/(routes)/workspace/[fileId]/page.tsx
@@ -6,43 +6,58 @@ import { useConvex } from 'convex/react';
 import { api } from '@/convex/_generated/api';
 import { FILE } from '../../dashboard/_components/FileList';
 import Canvas from '../_components/Canvas';
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable"
 
-function Workspace({params}:any) {
-   const [triggerSave,setTriggerSave]=useState(false);
-   const convex=useConvex();
-   const [fileData,setFileData]=useState<FILE|any>();
-   useEffect(()=>{
-    console.log("FILEID",params.fileId)
-    params.fileId&&getFileData();
-   },[])
+function Workspace({ params }: any) {
+  const [triggerSave, setTriggerSave] = useState(false);
+  const convex = useConvex();
+  const [fileData, setFileData] = useState<FILE | any>();
+  useEffect(() => {
+    console.log("FILEID", params.fileId)
+    params.fileId && getFileData();
+  }, [])
 
-   const getFileData=async()=>{
-    const result=await convex.query(api.files.getFileById,{_id:params.fileId})
+  const getFileData = async () => {
+    const result = await convex.query(api.files.getFileById, { _id: params.fileId })
     setFileData(result);
   }
   return (
     <div>
-      <WorkspaceHeader onSave={()=>setTriggerSave(!triggerSave)} />
+      <WorkspaceHeader onSave={() => setTriggerSave(!triggerSave)} />
+
 
       {/* Workspace Layout  */}
-      <div className='grid grid-cols-1
-      md:grid-cols-2'>
-        {/* Document  */}
+      <ResizablePanelGroup
+        direction="horizontal"
+        className='grid grid-cols-1 md:grid-cols-2'
+      >
+        <ResizablePanel defaultSize={25}>
+
+          {/* Document  */}
           <div className=' h-screen'>
             <Editor onSaveTrigger={triggerSave}
-            fileId={params.fileId}
-            fileData={fileData}
+              fileId={params.fileId}
+              fileData={fileData}
             />
           </div>
-        {/* Whiteboard/canvas  */}
-        <div className=' h-screen border-l'>
+        </ResizablePanel>
+        <ResizableHandle withHandle />
+        <ResizablePanel defaultSize={75}>
+
+          {/* Whiteboard/canvas  */}
+          <div className=' h-screen border-l'>
             <Canvas
-             onSaveTrigger={triggerSave}
-             fileId={params.fileId}
-             fileData={fileData}
+              onSaveTrigger={triggerSave}
+              fileId={params.fileId}
+              fileData={fileData}
             />
-        </div>
-      </div>
+          </div>
+        </ResizablePanel>
+      </ResizablePanelGroup>
     </div>
   )
 }
