7abf60d6c5022ddb0f934848c87e8990b2fe0b82
yashdev9274 <yashdev.yvd@gmail.com>
2024-06-20 22:39:23 +0530

    ⭐ feat: update settings page.

--- app/(routes)/dashboard/settings/page.tsx
+++ app/(routes)/dashboard/settings/page.tsx
@@ -1,16 +1,53 @@
 import React from 'react'
+import Link from "next/link"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "@/components/ui/card"
+// import { Checkbox } from "@/components/ui/checkbox"
 
 const Settings = () => {
     return (
         <div className="space-y-6 gap-x-6 px-5">
-            <div>
-                <h3 className="text-lg font-medium">Profile</h3>
-                <p className="text-sm text-muted-foreground">
-                    This is profile setting page.
-                </p>
+            <div className="grid gap-6">
+                <Card x-chunk="dashboard-04-chunk-1">
+                    <CardHeader>
+                        <CardTitle>Team Name</CardTitle>
+                        <CardDescription>This is your team's visible name within D&D. </CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <form>
+                            <Input placeholder="Team Name" />
+                        </form>
+                    </CardContent>
+                    <CardFooter className="border-t px-6 py-4">
+                        <Button>Save</Button>
+                    </CardFooter>
+                </Card>
+                <Card x-chunk="dashboard-04-chunk-2">
+                    <CardHeader>
+                        <CardTitle>Plugins Directory</CardTitle>
+                        <CardDescription>The directory within your project, in which your plugins are located.</CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <form className="flex flex-col gap-4">
+                            <Input placeholder="Project Name" defaultValue="/content/plugins" />
+                            <div className="flex items-center space-x-2">
+                                {/* <Checkbox id="include" defaultChecked /> */}
+                                <label
+                                    htmlFor="include"
+                                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                                >
+                                    Allow administrators to change the directory.
+                                </label>
+                            </div>
+                        </form>
+                    </CardContent>
+                    <CardFooter className="border-t px-6 py-4">
+                        <Button>Save</Button>
+                    </CardFooter>
+                </Card>
             </div>
-            {/* <Separator /> */}
-            {/* <ProfileForm /> */}
+
         </div>
     )
 }
