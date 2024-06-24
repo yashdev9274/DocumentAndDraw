e70f7a595e82dd760e8f4b4ac995d49194ca43cf
yashdev9274 <yashdev.yvd@gmail.com>
2024-06-24 22:34:39 +0530

    🔧 update: billing section settings page.

--- app/(routes)/dashboard/settings/billing/page.tsx
+++ app/(routes)/dashboard/settings/billing/page.tsx
@@ -1,15 +1,36 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
 import { Card, CardHeader, CardTitle, CardDescription, CardContent } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
+import { Separator } from '@/components/ui/separator'
+import { ExternalLink } from 'lucide-react'
 import React from 'react'
 
 const BillingPage = () => {
     return (
         <div className="space-y-6 gap-x-6 px-5">
             <div className="grid gap-6">
-                <Card x-chunk="dashboard-04-chunk-1">
+                <Card className="overflow-hidden" x-chunk="dashboard-04-chunk-1">
                     <CardHeader>
                         <CardTitle>Billing</CardTitle>
                         <CardDescription>Manage all things billing. </CardDescription>
+                    </CardHeader>
+                    <Separator className="my-2" />
+                    <CardHeader className="flex flex-row items-start bg-muted/50">
+                        <div className="grid gap-0.5">
+
+                            <CardTitle className="group flex items-center gap-2 text-lg">View and manage your billing details</CardTitle>
+                            <CardDescription >View and edit your billing details, as well as cancel your subscription.</CardDescription>
+                        </div>
+                        <div className="ml-auto flex items-center gap-1">
+                            <Button size="sm" variant="outline" className="h-8 gap-1" >
+                                <span>
+                                    Billing Portal
+                                </span>
+                                <ExternalLink className="h-3.5 w-3.5" />
+                            </Button>
+                        </div>
                     </CardHeader>
                 </Card>
             </div>
