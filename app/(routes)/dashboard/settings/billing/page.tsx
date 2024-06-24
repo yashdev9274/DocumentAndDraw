9f72cd469dde1cc26c91b05aeb78eba9ecfc3b01
yashdev9274 <yashdev.yvd@gmail.com>
2024-06-24 22:45:46 +0530

    🔧 update: billing section settings page.

--- app/(routes)/dashboard/settings/billing/page.tsx
+++ app/(routes)/dashboard/settings/billing/page.tsx
@@ -24,11 +24,26 @@ const BillingPage = () => {
                             <CardDescription >View and edit your billing details, as well as cancel your subscription.</CardDescription>
                         </div>
                         <div className="ml-auto flex items-center gap-1">
-                            <Button size="sm" variant="outline" className="h-8 gap-1" >
+                            <Button size="sm" variant="outline" className="h-8 gap-1 bg-zinc-600 hover:bg-zinc-700 text-white" >
                                 <span>
                                     Billing Portal
                                 </span>
                                 <ExternalLink className="h-3.5 w-3.5" />
+                            </Button>
+                        </div>
+                    </CardHeader>
+                    <Separator className="my-2" />
+                    <CardHeader className="flex flex-row items-start bg-muted/50">
+                        <div className="grid gap-0.5">
+
+                            <CardTitle className="group flex items-center gap-2 text-lg">Need anything else?</CardTitle>
+                            <CardDescription >If you need any further help with billing, our support team are here to help.</CardDescription>
+                        </div>
+                        <div className="ml-auto flex items-center gap-1">
+                            <Button size="sm" variant="outline" className="h-8 gap-1" >
+                                <span>
+                                    Contact Support
+                                </span>
                             </Button>
                         </div>
                     </CardHeader>
